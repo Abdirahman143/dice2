fdcc07d1fe3fafe28fd23833f67f38b914b7e99c
Abdirahman143 <updulrahman1992@gmail.com>
2020-04-27 14:25:32 +0300

    Dice game in flutter

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  
+  runApp(
+    
+    MaterialApp(
+      home: Scaffold(
+         backgroundColor:  Colors.redAccent,
+        appBar:  AppBar(
+           backgroundColor: Colors.pinkAccent,
+           titleSpacing: 1.5,
+          centerTitle: true,
+          title: Text('Ladhuu'),
+          
+        ),
+        
+        body: DicePage(),
+      )
+      
+    )
+    
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDice =1;
+  int rightDice =1;
+  
+  void ChangeFace()
+  {
+    setState(() {
+      LeftDice =Random().nextInt(6)+1;
+      rightDice= Random().nextInt(6)+1;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                  onPressed:(){
+                    ChangeFace();
+              },
+                  child: Image.asset('images/dice$LeftDice.png')
+              ),
+          ),
+          Expanded(
+              child: FlatButton(
+                  onPressed:(){
+                    ChangeFace();
+                  },
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
